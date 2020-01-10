db0f491aaf77e3ba7bb602d6619005d320cfa9b5
signalwerk <sh@signalwerk.ch>
2020-01-10 01:35:45 +0100

    MOD: checkin data

--- example/index.js
+++ example/index.js
@@ -2,30 +2,38 @@ var remark = require("remark");
 var gatsbyTOC = require("../");
 const fs = require("fs");
 
-let data = fs.readFileSync("./test.md");
-
-const toc = () => {
-  return markdownAST => {
-    fs.writeFileSync(
-      "./data-before.json",
-      JSON.stringify(markdownAST, null, 2)
-    );
-    gatsbyTOC(
-      { markdownAST },
-      {
-        exclude: "Table of Contents",
-        tight: false,
-        fromHeading: 1,
-        toHeading: 6
-      }
-    );
-    fs.writeFileSync("./data-after.json", JSON.stringify(markdownAST, null, 2));
+let writeSnapshots = testName => {
+  let data = fs.readFileSync(`./tests/${testName}/in.md`);
+  const toc = () => {
+    return markdownAST => {
+      fs.writeFileSync(
+        `./tests/${testName}/snapshots/data-before.json`,
+        JSON.stringify(markdownAST, null, 2)
+      );
+      gatsbyTOC(
+        { markdownAST },
+        {
+          exclude: "Table of Contents",
+          tight: false,
+          fromHeading: 1,
+          toHeading: 6
+        }
+      );
+      fs.writeFileSync(
+        `./tests/${testName}/snapshots/data-after.json`,
+        JSON.stringify(markdownAST, null, 2)
+      );
+    };
   };
+
+  remark()
+    .use(toc)
+    .process(data, function(err, file) {
+      if (err) throw err;
+      // console.log(String(file));
+      fs.writeFileSync(`./tests/${testName}/snapshots/data-out.md`, String(file));
+    });
 };
 
-remark()
-  .use(toc)
-  .process(data, function(err, file) {
-    if (err) throw err;
-    console.log(String(file));
-  });
+writeSnapshots("single-excludes");
+writeSnapshots("multiple-excludes");
